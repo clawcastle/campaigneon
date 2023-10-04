4f235a7c8d8c892e05f4a679750e7b7a1aca01a1
anderskloborg <anderskloborg@gmail.com>
2023-10-04 22:36:53 +0000

    Adds dependencies

--- src/main.py
+++ src/main.py
@@ -1,1 +1,11 @@
-print("main")
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/hello_world")
+def hello_world():
+    return "hello world"
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=5000)
