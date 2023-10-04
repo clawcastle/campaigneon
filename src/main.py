b462671f9efbeb99939d1dccb39ee857a83a410c
anderskloborg <anderskloborg@gmail.com>
2023-10-04 22:45:51 +0000

    Adds graphql dependency with sample query

--- src/main.py
+++ src/main.py
@@ -1,7 +1,30 @@
 from fastapi import FastAPI
 import uvicorn
+import strawberry
+from strawberry.fastapi import GraphQLRouter
+from uuid import UUID, uuid4
+from typing import List
+
+@strawberry.type
+class Campaign:
+    id: UUID
+    title: str
+
+@strawberry.type
+class Query:
+    @strawberry.field
+    def campaigns(self) -> List[Campaign]:
+        campaigns = [Campaign(id=uuid4(), title="campaign 1"), Campaign(id=uuid4(), title="campaign 2")]
+
+        return campaigns
+    
+
+schema = strawberry.Schema(Query)
+
+graphql_app = GraphQLRouter(schema)
 
 app = FastAPI()
+app.include_router(graphql_app, prefix="/graphql")
 
 @app.get("/hello_world")
 def hello_world():
