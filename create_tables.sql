122c17cc717fde0156429192eb200ea7e174b9b1
sunero4 <anderskloborg@gmail.com>
2023-10-08 13:16:42 +0000

    Add create script for entry table.

--- create_tables.sql
+++ create_tables.sql
@@ -10,7 +10,7 @@ CREATE TABLE IF NOT EXISTS public.users
 CREATE TABLE IF NOT EXISTS public.campaigns
 (
     id uuid NOT NULL,
-    title text COLLATE pg_catalog."default" NOT NULL,
+    title text NOT NULL,
     created_at timestamp without time zone NOT NULL,
     created_by text NOT NULL,
     CONSTRAINT campaigns_pkey PRIMARY KEY (id),
@@ -36,4 +36,24 @@ CREATE TABLE IF NOT EXISTS public.categories
     parent_id uuid,
     CONSTRAINT categories_pkey PRIMARY KEY (id),
     CONSTRAINT campaign_id_fk FOREIGN KEY (campaign_id) REFERENCES public.campaigns (id)
+);
+
+CREATE TABLE IF NOT EXISTS public.entries
+(
+    id uuid NOT NULL,
+    campaign_id uuid NOT NULL,
+    title text NOT NULL,
+    entry_text_rich text NOT NULL,
+    entry_text_raw text NOT NULL,
+    entry_text_summary text NOT NULL,
+    created_at timestamp without time zone NOT NULL,
+    last_modified_at timestamp without time zone NOT NULL,
+    created_by text NOT NULL,
+    last_modified_by text NOT NULL,
+    category_id uuid,
+    CONSTRAINT entries_pkey PRIMARY KEY (id),
+    CONSTRAINT campaign_id_fk FOREIGN KEY (campaign_id) REFERENCES public.campaigns(id),
+    CONSTRAINT created_by_fk FOREIGN KEY (created_by) REFERENCES public.users(id),
+    CONSTRAINT last_modified_by_fk FOREIGN KEY (last_modified_by) REFERENCES public.users(id),
+    CONSTRAINT category_id_fk FOREIGN KEY (category_id) REFERENCES public.categories(id),
 );
