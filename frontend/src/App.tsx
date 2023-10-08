e522040aa9bcb2356fb2e759349a2b12f442c7c2
sunero4 <anderskloborg@gmail.com>
2023-10-08 20:41:17 +0000

    Add auth0 dependency and follow their login example.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useAuth0 } from '@auth0/auth0-react'
 
-function App() {
-  const [count, setCount] = useState(0)
+const LoginButton = () => {
+  const { loginWithRedirect } = useAuth0();
+
+  return <button onClick={() => loginWithRedirect()}>Login</button>
+}
+
+const LogoutButton = () => {
+  const { logout } = useAuth0();
+
+  return <button onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>Logout</button>
+}
+
+const Profile = () => {
+  const { user, isAuthenticated, isLoading } = useAuth0();
+
+  if (isLoading) {
+    return <div>Loading ...</div>;
+  }
+
+  console.log(user);
+
 
   return (
-    <>
+    isAuthenticated && user && (
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <img src={user.picture} alt={user.name} />
+        <h2>{user.name}</h2>
+        <p>{user.email}</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    )
+  );
+}
+
+function App() {
+  return (<div>
+    <div>
+      <LoginButton />
+    </div>
+    <div>
+      <LogoutButton />
+    </div>
+    <div>
+      <Profile />
+    </div>
+  </div>)
 }
 
 export default App
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { Auth0Provider } from '@auth0/auth0-react'
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider domain="campaigneon.eu.auth0.com" clientId="0Hrfqnycryw5HJNCLUkYNIe9TW4txtjq" authorizationParams={{
+      redirect_uri: window.location.origin
+    }}>
+      <App />
+    </Auth0Provider>
   </React.StrictMode>,
 )
