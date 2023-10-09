159bc85a81ace526fddfccabe8f6d2b4bd6185d9
sunero4 <anderskloborg@gmail.com>
2023-10-09 14:49:41 +0000

    delete vite starter template boilerplate, add auth guard.

---
+++ frontend/src/components/AuthGuard.tsx
@@ -0,0 +1,16 @@
+import { useAuth0 } from '@auth0/auth0-react'
+import React from 'react';
+
+type AuthGuardProps = {
+    children?: React.ReactNode
+}
+
+export const AuthGuard: React.FC<AuthGuardProps> = ({ children }) => {
+    const { isAuthenticated } = useAuth0();
+
+    if (!isAuthenticated) {
+        return null;
+    }
+
+    return <>{children}</>
+}
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
 import { Auth0Provider } from '@auth0/auth0-react'
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
