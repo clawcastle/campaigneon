a4e323b880f1eb3a94d9ad791766fd4b753a5b82
anderskloborg <anderskloborg@gmail.com>
2023-10-09 20:52:45 +0000

    display campaign title

--- frontend/src/pages/Campaign.tsx
+++ frontend/src/pages/Campaign.tsx
@@ -1,3 +1,30 @@
+import { useQuery } from "@apollo/client";
+import { gql } from "../__generated__"
+import { useParams } from "react-router-dom";
+
+const FETCH_CAMPAIGNS_QUERY = gql(`
+                        query FetchCampaigns {
+                            campaigns {
+                                id
+                                title
+                            }
+                        }
+`);
+
 export const CampaignPage = () => {
-    return <div>campaign</div>
+    const { campaignId } = useParams();
+
+    const { data, loading } = useQuery(FETCH_CAMPAIGNS_QUERY);
+
+    if (loading) {
+        return <div>Loading...</div>
+    }
+
+    const campaign = data?.campaigns.find(campaign => campaign.id === campaignId);
+
+    if (!campaign) {
+        return <div>Campaign not found...</div>
+    }
+
+    return <h2>{campaign.title}</h2>
 }
