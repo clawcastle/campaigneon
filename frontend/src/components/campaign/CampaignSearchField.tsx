fa390538f2825704b29a176fbfc6bace1fe3bcd8
sunero4 <anderskloborg@gmail.com>
2023-10-17 19:15:30 +0000

    Started working on a search field for campaign items.

---
+++ frontend/src/components/campaign/CampaignSearchField.tsx
@@ -0,0 +1,34 @@
+import Fuse from "fuse.js";
+import { TextField } from "@mui/material"
+import React, { useContext, useMemo, useState } from "react";
+import { CampaignContext } from "../../context/CampaignContext";
+
+export const CampaignSearchField = () => {
+    const [query, setQuery] = useState("");
+
+    const { campaignItemsMetadata } = useContext(CampaignContext);
+
+    const searchTerms = useMemo(() => {
+        if (!campaignItemsMetadata) {
+            return [];
+        }
+
+        return campaignItemsMetadata.map(c => c.title);
+    }, [campaignItemsMetadata])
+
+    const fuse = useMemo(() => new Fuse(searchTerms), [searchTerms]);
+
+    const searchResults = useMemo(() => {
+        const results = fuse.search(query);
+
+        return results.map(r => r.item);
+    }, [fuse, query])
+
+    console.log(searchResults);
+
+    const onTextFieldChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setQuery(e.target.value);
+    }
+
+    return <TextField variant="outlined" onChange={onTextFieldChange} />
+}
--- frontend/src/components/common/SearchField.tsx
+++
@@ -1,5 +0,0 @@
-import { TextField } from "@mui/material"
-
-export const SearchField = () => {
-    return <TextField variant="outlined" />
-}
